abb12ea8c1b160c3d790ec67be768fb05646bb2c
fyagci9 <firatyagci50@gmail.com>
2024-04-09 23:33:22 +0300

    09-04-2024

---
+++ mvc-projecet/Controllers/AdminCategoryController.cs
@@ -0,0 +1,47 @@
+﻿using BussinesLayer.Concrete;
+using BussinesLayer.ValidationRules;
+using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_projecet.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryvalidator = new CategoryValidator();    
+            ValidationResult result = categoryvalidator.Validate(p); 
+            if (result.IsValid)
+            {
+                cm.CategoryAddBL(p);
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                }
+            }
+            return View();
+        }
+    }
+}
