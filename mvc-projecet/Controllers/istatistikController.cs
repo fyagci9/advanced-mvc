3fe200e9e24e3d6e495fa9612be1dcc30044595e
fyagci9 <firatyagci50@gmail.com>
2024-04-22 23:49:44 +0300

    22-04-2024

--- mvc-projecet/Controllers/istatistikController.cs
+++ mvc-projecet/Controllers/istatistikController.cs
@@ -14,11 +14,43 @@ namespace mvc_project.Controllers
     {
         // GET: istatistik
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
         public ActionResult Index()
         {
+
+            //toplam kategori sayısı
+            
             var CatCount = cm.GetList().Count;
             ViewBag.CatCount = CatCount;
-            var a=cm.GetList();
+
+            //yazılım başlığı kategorisi
+            var softwareCategory = cm.GetList().FirstOrDefault(x=>x.CategoryName=="yazılım");
+            var softwareHeadingCount = hm.GetList().Count(x => x.CategoryID == softwareCategory.CategoryID);
+            ViewBag.SoftwareHeadingCount = softwareHeadingCount;
+
+            //a  sayısı
+            var aLetter = wm.GetList().Count(x=>x.WriterName.Contains("a"));
+            ViewBag.aLetter = aLetter;
+
+            //en fazla başlığa sahip kategori adı
+            var headingList = hm.GetList()
+                .GroupBy(x => x.CategoryID)
+                .Select(x => new { CategoryId = x.Key, Count = x.Count() })
+                .OrderByDescending(x => x.Count)
+                .FirstOrDefault();
+            var mostHeadingCategory = cm.GetByID(headingList.CategoryId).CategoryName;
+            ViewBag.mostHeadingCategory = mostHeadingCategory;
+
+            //true-false farkı
+
+            var trueValue = cm.GetList().Count(x => x.CategoryStatus == true);
+            var falseValue = cm.GetList().Count(x => x.CategoryStatus == false);
+            var distanceValues = trueValue - falseValue;
+            ViewBag.distanceValues = distanceValues;
+
+
+
             return View();
         }
     }
