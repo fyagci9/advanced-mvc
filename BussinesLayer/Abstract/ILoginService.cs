b1fc1de997ed1c87452e6ec1175518ce39ff4a5f
fyagci9 <firatyagci50@gmail.com>
2024-05-07 23:26:40 +0300

    07-05-2024

---
+++ BussinesLayer/Abstract/ILoginService.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Abstract
+{
+    public interface ILoginService
+    {
+        List<Admin> GetList();
+        void adminAdd(Admin admin);
+
+      
+    }
+}
---
+++ BussinesLayer/Concrete/LoginManager.cs
@@ -0,0 +1,31 @@
+﻿using BussinesLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Concrete
+{
+    public class LoginManager:ILoginService
+    {
+        ILoginDal _loginDal;
+
+        public LoginManager(ILoginDal loginDal)
+        {
+            _loginDal = loginDal;
+        }
+
+        public void adminAdd(Admin admin)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Admin> ILoginService.GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
