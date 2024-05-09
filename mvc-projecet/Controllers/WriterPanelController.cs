88d8bfd005e6d6b07cbea5a92028c3c36cad005b
fyagci9 <firatyagci50@gmail.com>
2024-05-09 23:37:59 +0300

    09-05-2024

--- mvc-projecet/Controllers/WriterPanelController.cs
+++ mvc-projecet/Controllers/WriterPanelController.cs
@@ -1,5 +1,6 @@
 ﻿using BussinesLayer.Concrete;
 using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace mvc_projecet.Controllers
     {
         // GET: WriterPanel
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
 
         public ActionResult WriterProfile()
         {
@@ -22,6 +24,64 @@ namespace mvc_projecet.Controllers
            
             var values = hm.GetListByWriter();
             return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString(),
+
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Now;
+            p.HeadingStatus = true;
+            p.WriterID = 4;
+            hm.headingAdd(p);
+            return RedirectToAction("MyHeading");
+            
+        }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString(),
+
+                                                  }).ToList();
+
+            ViewBag.vlc = valuecategory;
+
+            var headingValues = hm.GetByID(id);
+            return View(headingValues);
+
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.headingUpdate(p);
+            return RedirectToAction("MyHeading");
+        }
+        public ActionResult DeleteHeading(int id)
+        {
+            var HeadingValue = hm.GetByID(id);
+            HeadingValue.HeadingStatus = false;
+            hm.headingDelete(HeadingValue);
+            return RedirectToAction("MyHeading");
+
+
         }
     }
 }
---
+++ mvc-projecet/Controllers/WriterPanelMessageController.cs
@@ -0,0 +1,35 @@
+﻿using BussinesLayer.Concrete;
+using BussinesLayer.ValidationRules;
+using DataAccessLayer.EntitiyFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_projecet.Controllers
+{
+    public class WriterPanelMessageController : Controller
+    {
+
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        MessagesValidator messagevalidator = new MessagesValidator();
+
+        // GET: WriterPanelMessage
+        public ActionResult Inbox()
+        {
+            var messageList = mm.GetListInbox();
+            return View(messageList);
+        }
+        public ActionResult Sendbox()
+        {
+            var messageList = mm.GetListSendbox();
+            return View(messageList);
+        }
+        public PartialViewResult MessageListMenu()
+        {
+            return PartialView();
+        }
+
+    }
+}
